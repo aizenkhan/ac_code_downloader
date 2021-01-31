9fdd8c2001f08ed5f523d3c001c4bab46a9c9345
aizenkhan <altamashjahangirkhan123@gmail.com>
2021-01-31 21:36:28 +0530

    updated constants.py file

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -1,21 +1,33 @@
 # constants related to cses parser
 
 # form constants
-CSES_USERNAME_FORM_NAME="nick"
-CSES_PASSWORD_FORM_NAME="pass"
-CSES_CSRF_TOKEN_FORM_NAME="csrf_token"
+CSES_USERNAME_FORM_NAME = "nick"
+CSES_PASSWORD_FORM_NAME = "pass"
+CSES_CSRF_TOKEN_FORM_NAME = "csrf_token"
+
+# table attribute constants
+CSES_TABLE_CLASS = "wide"
+CSES_TABLE_SUBMISSION_COL = "submission"
+CSES_TABLE_VERDICT_COL = "result"
+CSES_TABLE_TIMESTAMP_COL = "time"
+CSES_TABLE_CODE_LANG_COL = "lang"
+CSES_TABLE_CODE_TIME_COL = "code time"
+CSES_TABLE_CODE_SIZE_COL = "code size"
 
 # other page constants
-CSES_ACCOUNT_ANCHOR_CLASS="account"
-CSES_AC_PROBLEM_CLASS="task-score icon full"
+CSES_ACCOUNT_ANCHOR_CLASS = "account"
+CSES_AC_PROBLEM_CLASS = "task-score icon full"
+CSES_PAGE_SPAN_CLASS = "pager wide"
+CSES_NEXT_PAGE_CLASS = "next"
+CSES_PREV_PAGE_CLASS = "prev"
 
 # yaml constants
-CSES_YAML_REL_PATH="../../files/cses.yaml"
-CSES_USERNAME_YAML_NAME="username"
-CSES_PASSWORD_YAML_NAME="password"
+CSES_YAML_REL_PATH = "../../files/cses.yaml"
+CSES_USERNAME_YAML_NAME = "username"
+CSES_PASSWORD_YAML_NAME = "password"
 
 # url constants
-CSES_BASE_URL="https://cses.fi"
-CSES_LOGIN_URL="https://cses.fi/login"
-CSES_PROBLEMS_URL="https://cses.fi/problemset"
-CSES_PROBLEM_RESULTS_URL="https://cses.fi/problemset/view"
+CSES_BASE_URL = "https://cses.fi"
+CSES_LOGIN_URL = "https://cses.fi/login"
+CSES_PROBLEMS_URL = "https://cses.fi/problemset"
+CSES_PROBLEM_RESULTS_URL = "https://cses.fi/problemset/view"
